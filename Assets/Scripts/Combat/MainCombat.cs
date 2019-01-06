5fcf3348f3ea8f2d95ae99efdff11f252a0657d5
dupuqub <dupuqub@gmail.com>
2019-01-06 20:21:18 -0200

    first commit

---
+++ Assets/Scripts/Combat/MainCombat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCombat : MonoBehaviour
+{
+  void Start()
+  {
+    //
+  }
+
+  void Update()
+  {
+    //
+  }
+}
